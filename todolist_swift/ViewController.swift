ca18c2245547f7012e311f52e37b1615fb29d833
yasuhirokojima <yasugigabaitointel@gmail.com>
2015-06-27 10:32:37 +0900

    Initial Commit

---
+++ todolist_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todolist_swift
+//
+//  Created by 小島康寛 on 2015/06/27.
+//  Copyright (c) 2015年 yasuhirokojima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ todolist_swiftTests/todolist_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  todolist_swiftTests.swift
+//  todolist_swiftTests
+//
+//  Created by 小島康寛 on 2015/06/27.
+//  Copyright (c) 2015年 yasuhirokojima. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class todolist_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
